fec6bd4fcf2d795e0adb0c7f176b5200751e25bf
Joshua <joshua.omundson@gmail.com>
2021-12-02 15:46:58 -0700

    Moves logic to code behind for task_all

--- src/BugTracker.Web/tasks_all.aspx.cs
+++ src/BugTracker.Web/tasks_all.aspx.cs
@@ -1,11 +1,41 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Data;
 using System.Web;
 using System.Web.UI;
+using btnet;
+
 namespace BugTracker.Web
 {
     public partial class tasks_all : Page
     {
-    }
+		public DataSet ds_tasks;
+
+		public Security security;
+
+		void Page_Init(object sender, EventArgs e) { ViewStateUserKey = Session.SessionID; }
+
+		void Page_Load(Object sender, EventArgs e)
+		{
+
+			Util.do_not_cache(Response);
+
+			security = new Security();
+			security.check_security(HttpContext.Current, Security.ANY_USER_OK);
+
+			titl.InnerText = Util.get_setting("AppTitle", "BugTracker.NET") + " - "
+					+ "all tasks";
+
+			if (security.user.is_admin || security.user.can_view_tasks)
+			{
+				// allowed
+			}
+			else
+			{
+				Response.Write("You are not allowed to view tasks");
+				Response.End();
+			}
+
+			ds_tasks = btnet.Util.get_all_tasks(security, 0);
+		}
+	}
 }
