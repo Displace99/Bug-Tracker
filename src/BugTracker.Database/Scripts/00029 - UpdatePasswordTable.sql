5497ac6d4ecf2dec3fc5e5385fa6eb670cbf3533
Joshua Omundson <joshua.omundson@gmail.com>
2022-10-28 09:26:32 -0600

    Updates database tables with new password hashing

---
+++ src/BugTracker.Database/Scripts/00029 - UpdatePasswordTable.sql
@@ -0,0 +1,5 @@
+﻿ALTER TABLE users add password_reset_key nvarchar(200);
+ALTER TABLE users alter column us_salt nvarchar(120);
+ALTER TABLE emailed_links alter column el_salt nvarchar(120);
+ALTER TABLE users alter column us_password nvarchar(max);
+ALTER TABLE emailed_links alter column el_password nvarchar(max);
