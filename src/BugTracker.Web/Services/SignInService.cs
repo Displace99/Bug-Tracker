c478ced37704be107f2d0334f405e2c24833ce53
Joshua Omundson <joshua.omundson@gmail.com>
2022-11-03 10:58:07 -0600

    Adds Services for Sign In workflow

---
+++ src/BugTracker.Web/Services/SignInService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace BugTracker.Web.Services
+{
+    public class SignInService
+    {
+        public UserService userService = new UserService();
+
+        public bool ValidateCustomer(string userName, string password)
+        {
+            //Checks to see if the user exists. If not return false
+            DataRow dr = userService.FindByUserName(userName);
+
+            if(dr == null)
+            {
+                return false;
+            }
+
+            //If user exists, try authenticating their password
+            return btnet.Authenticate.check_password(userName, password);
+        }
+    }
+}
---
+++ src/BugTracker.Web/Services/UserService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace BugTracker.Web.Services
+{
+    public class UserService
+    {
+        public DataRow FindByUserName(string userName)
+        {
+            string sql = "select us_id from users where us_username = @userName";
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = sql;
+            cmd.Parameters.AddWithValue("@userName", userName);
+
+            return btnet.DbUtil.get_datarow(cmd);
+        }
+    }
+}
