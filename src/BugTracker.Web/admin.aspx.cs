7f49151d9f041c95e28ef5abef4086b7e5aeb874
Joshua Omundson <joshua.omundson@gmail.com>
2022-11-06 12:52:06 -0700

    Removes unused code

--- src/BugTracker.Web/admin.aspx.cs
+++ src/BugTracker.Web/admin.aspx.cs
@@ -1,17 +1,14 @@
 using btnet;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.UI;
+
 namespace BugTracker.Web
 {
     public partial class admin : Page
     {
 		public Security security;
-		public bool nag = false;
 
-		///////////////////////////////////////////////////////////////////////
 		void Page_Load(Object sender, EventArgs e)
 		{
 
@@ -22,18 +19,6 @@ namespace BugTracker.Web
 
 			title.InnerText = String.Format("{0} - admin", Util.get_setting("AppTitle", "BugTracker.NET")); 
 
-			if (false) // change this to if(true) to make the donation nag message go away
-			{
-
-			}
-			else
-			{
-				int bugs = Convert.ToInt32(btnet.DbUtil.execute_scalar("select count(1) from bugs"));
-				if (bugs > 3)
-				{
-					nag = true;
-				}
-			}
 		}
 	}
 }
