9971c8c16cc541f9f695cdde49b278e4ee51b0a8
Joshua Omundson <joshua.omundson@gmail.com>
2022-11-21 15:26:06 -0700

    Uses new service class

--- src/BugTracker.Web/priorities.aspx.cs
+++ src/BugTracker.Web/priorities.aspx.cs
@@ -1,4 +1,5 @@
 using btnet;
+using BugTracker.Web.Services.Priority;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -10,12 +11,12 @@ namespace BugTracker.Web
     public partial class priorities : Page
     {
         protected DataSet ds;
-
         protected Security security;
+
+        private PriorityService _priorityService = new PriorityService();
 
         void Page_Load(Object sender, EventArgs e)
         {
-
             Util.do_not_cache(Response);
 
             security = new Security();
@@ -23,15 +24,7 @@ namespace BugTracker.Web
 
             title.InnerText = String.Format("{0} - Priorities", Util.get_setting("AppTitle", "BugTracker.NET"));
 
-            ds = btnet.DbUtil.get_dataset(
-                @"select pr_id [id],
-		pr_name [description],
-		pr_sort_seq [sort seq],
-		'<div style=''background:' + pr_background_color + ';''>' + pr_background_color + '</div>' [background<br>color],
-		pr_style [css<br>class],
-		case when pr_default = 1 then 'Y' else 'N' end [default],
-		pr_id [hidden] from priorities");
-
+            ds = _priorityService.GetPriorityList();
         }
     }
 }
