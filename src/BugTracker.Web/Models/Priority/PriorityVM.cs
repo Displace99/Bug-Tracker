81267e664a8ae25652439f7a0a1ab7def9eff1b0
Joshua Omundson <joshua.omundson@gmail.com>
2022-11-22 13:26:54 -0700

    Adds Priority View Model

---
+++ src/BugTracker.Web/Models/Priority/PriorityVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTracker.Web.Models.Priority
+{
+    public class PriorityVM
+    {
+        public int PriorityId { get; set; }
+        public string Name { get; set; }
+        public string SortSequence { get; set; }
+        public string BackgroundColor { get; set; }
+        public string Style { get; set; }
+        public bool IsDefault { get; set; }
+    }
+}
