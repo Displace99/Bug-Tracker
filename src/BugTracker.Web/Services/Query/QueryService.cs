5448aad6ba6885bd20ca9ab7a199c4b52dc15e16
Joshua Omundson <joshua.omundson@gmail.com>
2023-01-12 12:36:45 -0700

    Moves individual user query to Service Layer

--- src/BugTracker.Web/Services/Query/QueryService.cs
+++ src/BugTracker.Web/Services/Query/QueryService.cs
@@ -12,6 +12,12 @@ namespace BugTracker.Web.Services.Query
 {
     public class QueryService
     {
+        /// <summary>
+        /// Returns a DataSet of all Queries in the system
+        /// </summary>
+        /// <param name="showAll">Show all queries or only global</param>
+        /// <param name="userId">User Id of user to show queries</param>
+        /// <returns>DataSet</returns>
         public DataSet GetAllUsersQueries(bool showAll, int userId)
         {
             StringBuilder sql = new StringBuilder();
@@ -43,28 +49,34 @@ namespace BugTracker.Web.Services.Query
             cmd.Parameters.AddWithValue("@userId", userId);
 
             return DbUtil.get_dataset(cmd);
+        }
 
-            //sql = @"select
-            //qu_desc [query],
-            //case
-            //	when isnull(qu_user,0) = 0 and isnull(qu_org,0) is null then 'everybody'
-            //	when isnull(qu_user,0) <> 0 then 'user:' + us_username
-            //	when isnull(qu_org,0) <> 0 then 'org:' + og_name
-            //	else ' '
-            //	end [visibility],
-            //'<a href=bugs.aspx?qu_id=' + convert(varchar,qu_id) + '>view list</a>' [view list],
-            //'<a target=_blank href=print_bugs.aspx?qu_id=' + convert(varchar,qu_id) + '>print list</a>' [print list],
-            //'<a target=_blank href=print_bugs.aspx?format=excel&qu_id=' + convert(varchar,qu_id) + '>export as excel</a>' [export as excel],
-            //'<a target=_blank href=print_bugs2.aspx?qu_id=' + convert(varchar,qu_id) + '>print detail</a>' [print list<br>with detail],
-            //'<a href=edit_query.aspx?id=' + convert(varchar,qu_id) + '>edit</a>' [edit],
-            //'<a href=delete_query.aspx?id=' + convert(varchar,qu_id) + '>delete</a>' [delete]
-            //from queries
-            //left outer join users on qu_user = us_id
-            //left outer join orgs on qu_org = og_id
-            //where 1 = $all /* all */
-            //or isnull(qu_user,0) = $us
-            //or isnull(qu_user,0) = 0
-            //order by qu_desc";
+        /// <summary>
+        /// Returns a DataSet of all queries for specific user
+        /// </summary>
+        /// <param name="userId">UserId to filter on</param>
+        /// <returns>DataSet</returns>
+        public DataSet GetQueriesByUserId(int userId)
+        {
+            StringBuilder sql = new StringBuilder();
+            sql.Append("SELECT qu_desc [query],");
+            sql.AppendLine("'<a href=bugs.aspx?qu_id=' + convert(varchar,qu_id) + '>view list</a>' [view list],");
+            sql.AppendLine("'<a target=_blank href=print_bugs.aspx?qu_id=' + convert(varchar,qu_id) + '>print list</a>' [print list],");
+            sql.AppendLine("'<a target=_blank href=print_bugs.aspx?format=excel&qu_id=' + convert(varchar,qu_id) + '>export as excel</a>' [export as excel],");
+            sql.AppendLine("'<a target=_blank href=print_bugs2.aspx?qu_id=' + convert(varchar,qu_id) + '>print detail</a>' [print list<br>with detail],");
+            sql.AppendLine("'<a href=edit_query.aspx?id=' + convert(varchar,qu_id) + '>rename</a>' [rename],");
+            sql.AppendLine("'<a href=delete_query.aspx?id=' + convert(varchar,qu_id) + '>delete</a>' [delete]");
+            sql.AppendLine("FROM queries");
+            sql.AppendLine("INNER JOIN users ON qu_user = us_id");
+            sql.AppendLine("WHERE isnull(qu_user,0) = @userId");
+            sql.AppendLine("ORDER BY qu_desc");
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = sql.ToString();
+
+            cmd.Parameters.AddWithValue("@userId", userId);
+
+            return DbUtil.get_dataset(cmd);
         }
 
         /// <summary>
--- src/BugTracker.Web/queries.aspx.cs
+++ src/BugTracker.Web/queries.aspx.cs
@@ -28,8 +28,6 @@ namespace BugTracker.Web
 			titl.InnerText = Util.get_setting("AppTitle", "BugTracker.NET") + " - "
 				+ "queries";
 
-			string sql = "";
-
 			int userId = security.user.usid;
 			bool showAll = show_all.Checked;
 
@@ -37,56 +35,12 @@ namespace BugTracker.Web
 			{
 				// allow admin to edit all queries
 				ds = _queryService.GetAllUsersQueries(showAll, userId);
-
-				//sql = @"select
-				//	qu_desc [query],
-				//	case
-				//		when isnull(qu_user,0) = 0 and isnull(qu_org,0) is null then 'everybody'
-				//		when isnull(qu_user,0) <> 0 then 'user:' + us_username
-				//		when isnull(qu_org,0) <> 0 then 'org:' + og_name
-				//		else ' '
-				//		end [visibility],
-				//	'<a href=bugs.aspx?qu_id=' + convert(varchar,qu_id) + '>view list</a>' [view list],
-				//	'<a target=_blank href=print_bugs.aspx?qu_id=' + convert(varchar,qu_id) + '>print list</a>' [print list],
-				//	'<a target=_blank href=print_bugs.aspx?format=excel&qu_id=' + convert(varchar,qu_id) + '>export as excel</a>' [export as excel],
-				//	'<a target=_blank href=print_bugs2.aspx?qu_id=' + convert(varchar,qu_id) + '>print detail</a>' [print list<br>with detail],
-				//	'<a href=edit_query.aspx?id=' + convert(varchar,qu_id) + '>edit</a>' [edit],
-				//	'<a href=delete_query.aspx?id=' + convert(varchar,qu_id) + '>delete</a>' [delete]
-				//	from queries
-				//	left outer join users on qu_user = us_id
-				//	left outer join orgs on qu_org = og_id
-				//	where 1 = $all /* all */
-				//	or isnull(qu_user,0) = $us
-				//	or isnull(qu_user,0) = 0
-				//	order by qu_desc";
-
-				//sql = sql.Replace("$all", show_all.Checked ? "1" : "0");
 			}
 			else
 			{
 				// allow editing for users' own queries
-
-				sql = @"select
-					qu_desc [query],
-					'<a href=bugs.aspx?qu_id=' + convert(varchar,qu_id) + '>view list</a>' [view list],
-					'<a target=_blank href=print_bugs.aspx?qu_id=' + convert(varchar,qu_id) + '>print list</a>' [print list],
-					'<a target=_blank href=print_bugs.aspx?format=excel&qu_id=' + convert(varchar,qu_id) + '>export as excel</a>' [export as excel],
-					'<a target=_blank href=print_bugs2.aspx?qu_id=' + convert(varchar,qu_id) + '>print detail</a>' [print list<br>with detail],
-					'<a href=edit_query.aspx?id=' + convert(varchar,qu_id) + '>rename</a>' [rename],
-					'<a href=delete_query.aspx?id=' + convert(varchar,qu_id) + '>delete</a>' [delete]
-					from queries
-					inner join users on qu_user = us_id
-					where isnull(qu_user,0) = $us
-					order by qu_desc";
-
-                sql = sql.Replace("$us", Convert.ToString(security.user.usid));
-				ds = btnet.DbUtil.get_dataset(sql);
-
+				ds = _queryService.GetQueriesByUserId(userId);
             }
-
-			//sql = sql.Replace("$us", Convert.ToString(security.user.usid));
-			//ds = btnet.DbUtil.get_dataset(sql);
-
 		}
 	}
 }
