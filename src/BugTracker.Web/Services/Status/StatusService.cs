fb11527810f6e0991467c34263754a08ccc20cf4
Joshua Omundson <joshua.omundson@gmail.com>
2023-02-03 10:54:15 -0700

    Moves sql to Service Layer

---
+++ src/BugTracker.Web/Services/Status/StatusService.cs
@@ -0,0 +1,23 @@
+﻿using btnet;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Permissions;
+using System.Web;
+
+namespace BugTracker.Web.Services.Status
+{
+    public class StatusService
+    {
+        public DataSet GetStatusList()
+        {
+            string sql = @"select st_id [id], st_name [status], st_sort_seq [sort seq], st_style [css<br>class],
+		        case when st_default = 1 then 'Y' else 'N' end [default],
+		        st_id [hidden]
+		        from statuses order by st_sort_seq";
+
+            return DbUtil.get_dataset(sql);
+        }
+    }
+}
--- src/BugTracker.Web/statuses.aspx.cs
+++ src/BugTracker.Web/statuses.aspx.cs
@@ -1,4 +1,5 @@
 using btnet;
+using BugTracker.Web.Services.Status;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -10,12 +11,12 @@ namespace BugTracker.Web
     public partial class statuses : Page
     {
         protected DataSet ds;
-
         protected Security security;
+
+        private StatusService _statusService = new StatusService();
 
         void Page_Load(Object sender, EventArgs e)
         {
-
             Util.do_not_cache(Response);
 
             security = new Security();
@@ -25,15 +26,7 @@ namespace BugTracker.Web
             titl.InnerText = Util.get_setting("AppTitle", "BugTracker.NET") + " - "
                 + "statuses";
 
-            ds = btnet.DbUtil.get_dataset(
-                @"select st_id [id],
-		st_name [status],
-		st_sort_seq [sort seq],
-		st_style [css<br>class],
-		case when st_default = 1 then 'Y' else 'N' end [default],
-		st_id [hidden]
-		from statuses order by st_sort_seq");
-
+            ds = _statusService.GetStatusList();
         }
 
     }
