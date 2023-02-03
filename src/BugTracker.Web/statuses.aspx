252294ef99f9fe75f08a666d8e168fc67ab6d305
Joshua Omundson <joshua.omundson@gmail.com>
2023-02-03 10:51:20 -0700

    Moves code to code behind

--- src/BugTracker.Web/statuses.aspx
+++ src/BugTracker.Web/statuses.aspx
@@ -3,41 +3,9 @@
 Copyright 2002-2014 Corey Trager
 Distributed under the terms of the GNU General Public License
 -->
-<!-- #include file = "inc.aspx" -->
 
-<script language="C#" runat="server">
-
-
-DataSet ds;
-
-Security security;
-
-void Page_Load(Object sender, EventArgs e)
-{
-
-	Util.do_not_cache(Response);
-	
-	security = new Security();
-	security.check_security( HttpContext.Current, Security.MUST_BE_ADMIN);
-
-
-	titl.InnerText = Util.get_setting("AppTitle","BugTracker.NET") + " - "
-		+ "statuses";
-
-	ds = btnet.DbUtil.get_dataset (
-		@"select st_id [id],
-		st_name [status],
-		st_sort_seq [sort seq],
-		st_style [css<br>class],
-		case when st_default = 1 then 'Y' else 'N' end [default],
-		st_id [hidden]
-		from statuses order by st_sort_seq");
-
-}
-
-
-
-</script>
+<%@ Import Namespace="btnet" %>
+<%@ Import Namespace="System.Data" %>
 
 <html>
 <head>
--- src/BugTracker.Web/statuses.aspx.cs
+++ src/BugTracker.Web/statuses.aspx.cs
@@ -1,5 +1,7 @@
+using btnet;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -7,5 +9,32 @@ namespace BugTracker.Web
 {
     public partial class statuses : Page
     {
+        protected DataSet ds;
+
+        protected Security security;
+
+        void Page_Load(Object sender, EventArgs e)
+        {
+
+            Util.do_not_cache(Response);
+
+            security = new Security();
+            security.check_security(HttpContext.Current, Security.MUST_BE_ADMIN);
+
+
+            titl.InnerText = Util.get_setting("AppTitle", "BugTracker.NET") + " - "
+                + "statuses";
+
+            ds = btnet.DbUtil.get_dataset(
+                @"select st_id [id],
+		st_name [status],
+		st_sort_seq [sort seq],
+		st_style [css<br>class],
+		case when st_default = 1 then 'Y' else 'N' end [default],
+		st_id [hidden]
+		from statuses order by st_sort_seq");
+
+        }
+
     }
 }
