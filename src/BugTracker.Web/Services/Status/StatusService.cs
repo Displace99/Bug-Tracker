e49d8a6840b3a0070bb40b2fb9e3ab568887f996
Joshua Omundson <joshua.omundson@gmail.com>
2023-02-03 15:47:55 -0700

    Moves sql logic to Service Layer

--- src/BugTracker.Web/Services/Status/StatusService.cs
+++ src/BugTracker.Web/Services/Status/StatusService.cs
@@ -2,8 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Security.Permissions;
+using System.Text;
 using System.Web;
 
 namespace BugTracker.Web.Services.Status
@@ -18,6 +20,39 @@ namespace BugTracker.Web.Services.Status
 		        from statuses order by st_sort_seq";
 
             return DbUtil.get_dataset(sql);
+        }
+
+        public DataRow GetStatusById(int Id)
+        {
+            string sql = @"select st_name, st_sort_seq, st_style, st_default from statuses where st_id = @Id";
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = sql;
+            cmd.Parameters.AddWithValue("@Id", Id);
+
+            return DbUtil.get_datarow(cmd);
+        }
+
+        public void DeleteStatus(int Id)
+        {
+            string sql = @"delete statuses where st_id = @statusId";
+
+            SqlCommand cmd = new SqlCommand(sql);
+            cmd.Parameters.AddWithValue("@statusId", Id);
+            
+            DbUtil.execute_nonquery(cmd);
+        }
+
+        public int GetBugCountByStatus(int Id)
+        {
+            string sql = "select count(1)[cnt] from bugs where bg_status = @statusId";
+
+            SqlCommand cmd = new SqlCommand(sql);
+            cmd.Parameters.AddWithValue("@statusId", Id);
+
+            DataRow dr = DbUtil.get_datarow(cmd);
+
+            return (int)dr["cnt"];
         }
     }
 }
--- src/BugTracker.Web/delete_status.aspx.cs
+++ src/BugTracker.Web/delete_status.aspx.cs
@@ -1,5 +1,10 @@
+using btnet;
+using BugTracker.Web.Models.Category;
+using BugTracker.Web.Services.Category;
+using BugTracker.Web.Services.Status;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -7,5 +12,51 @@ namespace BugTracker.Web
 {
     public partial class delete_status : Page
     {
+        String sql;
+
+        protected Security security;
+
+        private StatusService _statusService = new StatusService();
+
+        void Page_Init(object sender, EventArgs e) { ViewStateUserKey = Session.SessionID; }
+
+        void Page_Load(Object sender, EventArgs e)
+        {
+            Util.do_not_cache(Response);
+
+            security = new Security();
+            security.check_security(HttpContext.Current, Security.MUST_BE_ADMIN);
+
+            int id = 0;
+            Int32.TryParse(Request["id"], out id);
+
+            if (IsPostBack)
+            {
+                // do delete here
+                _statusService.DeleteStatus(id);
+                Server.Transfer("statuses.aspx");
+            }
+            else
+            {
+                titl.InnerText = Util.get_setting("AppTitle", "BugTracker.NET") + " - "
+                + "delete status";
+
+                int bugCount = _statusService.GetBugCountByStatus(id);
+                var statusDr = _statusService.GetStatusById(id);
+                string statusName = statusDr["st_name"].ToString();
+
+                if (bugCount > 0)
+                {
+                    Response.Write(string.Format("You can't delete status {0} because some bugs still reference it.", statusName));
+                    Response.End();
+                }
+                else
+                {
+                    confirm_href.InnerText = string.Format("Confirm delete of {0}", statusName);
+
+                    row_id.Value = id.ToString();
+                }
+            }
+        }
     }
 }
