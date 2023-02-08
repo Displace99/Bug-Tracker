ecaefa9d7127a76d51ae844c4f8d5d10efc62420
Joshua Omundson <joshua.omundson@gmail.com>
2023-02-08 16:25:48 -0700

    Moves logic to Service Layer

---
+++ src/BugTracker.Web/Services/UserDefinedFields/UserDefinedFieldService.cs
@@ -0,0 +1,22 @@
+﻿using btnet;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace BugTracker.Web.Services.UserDefinedFields
+{
+    public class UserDefinedFieldService
+    {
+        public DataSet GetFieldList()
+        {
+			string sql = @"select udf_id [id], udf_name [user defined attribute value], udf_sort_seq [sort seq],
+		        case when udf_default = 1 then 'Y' else 'N' end [default],
+		        udf_id [hidden]
+		        from user_defined_attribute order by udf_name";
+
+            return DbUtil.get_dataset(sql);
+        }
+    }
+}
--- src/BugTracker.Web/udfs.aspx.cs
+++ src/BugTracker.Web/udfs.aspx.cs
@@ -1,5 +1,8 @@
+using btnet;
+using BugTracker.Web.Services.UserDefinedFields;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -7,5 +10,22 @@ namespace BugTracker.Web
 {
     public partial class udfs : Page
     {
+        protected DataSet ds;
+        protected Security security;
+
+        private UserDefinedFieldService _udfService = new UserDefinedFieldService();
+
+        void Page_Load(Object sender, EventArgs e)
+        {
+            Util.do_not_cache(Response);
+
+            security = new Security();
+            security.check_security(HttpContext.Current, Security.MUST_BE_ADMIN);
+
+            title.InnerText = Util.get_setting("AppTitle", "BugTracker.NET") + " - "
+                + "user defined attribute values";
+
+            ds = _udfService.GetFieldList();
+        }
     }
 }
