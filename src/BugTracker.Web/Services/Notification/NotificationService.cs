3abd2efeecb80c5af4d84f4f29d45b2756355ede
Joshua Omundson <joshua.omundson@gmail.com>
2023-07-10 15:25:55 -0600

    Adds Notification Service

---
+++ src/BugTracker.Web/Services/Notification/NotificationService.cs
@@ -0,0 +1,29 @@
+﻿using btnet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTracker.Web.Services.Notification
+{
+    public class NotificationService
+    {
+        /// <summary>
+        /// Deletes all notificaitons with a status of "not sent"
+        /// </summary>
+        public void DeleteNotifications()
+        {
+            string sql = @"delete from queued_notifications where qn_status = N'not sent'";
+            DbUtil.execute_nonquery(sql);
+        }
+
+        /// <summary>
+        /// Updates retry count on all notificaitons with a status of "not sent"
+        /// </summary>
+        public void UpdateNotifications()
+        {
+            string sql = @"update queued_notifications set qn_retries = 0 where qn_status = N'not sent'";
+            DbUtil.execute_nonquery(sql);
+        }
+    }
+}
