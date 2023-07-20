4b575250abdaa47f5df08abae0ba0b946f302a58
Joshua Omundson <joshua.omundson@gmail.com>
2023-07-20 10:39:28 -0600

    Moves code to Search Service

---
+++ src/BugTracker.Web/Services/Search/SearchService.cs
@@ -0,0 +1,37 @@
+﻿using btnet;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace BugTracker.Web.Services.Search
+{
+    public class SearchService
+    {
+        public string GetSavedQuery(int queryId)
+        {
+            // use sql specified in query string
+            
+            string sql = @"select qu_sql from queries where qu_id = @queryId";
+            SqlCommand cmd = new SqlCommand(sql);
+            cmd.Parameters.AddWithValue("@queryId", queryId);
+
+            string bug_sql = (string)btnet.DbUtil.execute_scalar(cmd);
+
+            return bug_sql;
+        }
+
+        public DataSet FindBugsFromSavedQuery(int queryId, int userId, Security security)
+        {
+            string sql = GetSavedQuery(queryId);
+            
+            sql = sql.Replace("$ME", userId.ToString());
+            sql = Util.alter_sql_per_project_permissions(sql, security);
+
+            // all we really need is the bugid, but let's do the same query as print_bugs.aspx
+            return DbUtil.get_dataset(sql);
+        }
+    }
+}
--- src/BugTracker.Web/print_bugs2.aspx.cs
+++ src/BugTracker.Web/print_bugs2.aspx.cs
@@ -1,4 +1,5 @@
 using btnet;
+using BugTracker.Web.Services.Search;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -18,6 +19,8 @@ namespace BugTracker.Web
         protected bool images_inline;
         protected bool history_inline;
 
+        private SearchService _searchService = new SearchService();
+
         ///////////////////////////////////////////////////////////////////////
         void Page_Load(Object sender, EventArgs e)
         {
@@ -36,19 +39,9 @@ namespace BugTracker.Web
 
             if (qu_id_string != null)
             {
-
-                // use sql specified in query string
+                //// use sql specified in query string
                 int qu_id = Convert.ToInt32(qu_id_string);
-                sql = @"select qu_sql from queries where qu_id = $1";
-                sql = sql.Replace("$1", qu_id_string);
-                string bug_sql = (string)btnet.DbUtil.execute_scalar(sql);
-
-                // replace magic variables
-                bug_sql = bug_sql.Replace("$ME", Convert.ToString(security.user.usid));
-                bug_sql = Util.alter_sql_per_project_permissions(bug_sql, security);
-
-                // all we really need is the bugid, but let's do the same query as print_bugs.aspx
-                ds = btnet.DbUtil.get_dataset(bug_sql);
+                ds = _searchService.FindBugsFromSavedQuery(qu_id, security.user.usid, security);
             }
             else
             {
